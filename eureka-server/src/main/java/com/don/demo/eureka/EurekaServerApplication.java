5b052d1d127f50ed9f7662a9d4645545c904e29c
TJR_S <tjr.seeker@gmail.com>
2020-02-07 22:22:33 +0800

    spring-config通过的eureka注册中心进行config传递

---
+++ eureka-server/src/main/java/com/don/demo/eureka/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.don.demo.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//eureka注册中心
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
